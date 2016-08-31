d8ca0a3125de750d194d329b25eb88f4a3c54c12
tpetchel <tpetchel@gmail.com>
2016-08-31 08:58:25 -0400

    Add generated cookbook content

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'learn_chef_httpd'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures learn_chef_httpd'
+long_description 'Installs/Configures learn_chef_httpd'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: learn_chef_httpd
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
