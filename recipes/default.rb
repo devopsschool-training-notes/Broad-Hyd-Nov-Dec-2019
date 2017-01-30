03bdd45fe62a836d5415dbc6b4cf329ceaca4106
tpetchel <tpetchel@gmail.com>
2017-01-30 14:49:07 -0500

    Update for use with Chef Delivery
    + Add `issues_url` and `source_url` to metadata.md
    + Ignore FC033 (temporary)
    + Specify platform & version in default_spec.rb
    + Move default test to smoke directory

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,6 @@ service 'httpd' do
   action [:enable, :start]
 end
 
-template '/var/www/html/index.html' do
+template '/var/www/html/index.html' do # ~FC033
   source 'index.html.erb'
 end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -8,11 +8,9 @@
 unless os.windows?
   describe user('root') do
     it { should exist }
-    skip 'This is an example test, replace with your own test.'
   end
 end
 
 describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+  it { should be_listening }
 end
