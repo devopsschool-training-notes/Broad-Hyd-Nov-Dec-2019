628d08e7e6bef365010b550926469d3f669d0332
sreekanthpola1 <57520012+sreekanthpola1@users.noreply.github.com>
2019-12-20 17:35:14 +0530

    Update aws_sreekanth.tf

--- terraform/SreekanthPola/aws_sreekanth.tf
+++ terraform/SreekanthPola/aws_sreekanth.tf
@@ -9,25 +9,24 @@ resource "aws_security_group" "pola_sg" {
 
 
   ingress {
-    # TLS (change to whatever ports you need)
-    from_port   = 80
-    to_port     = 80
-    protocol    = "http"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
   }
-
-   ingress {
-    # TLS (change to whatever ports you need)
-    from_port   = 22
-    to_port     = 22
-    protocol    = "ssh"
+  ingress {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+  
   tags = {
     Name = "allow_all"
   }
