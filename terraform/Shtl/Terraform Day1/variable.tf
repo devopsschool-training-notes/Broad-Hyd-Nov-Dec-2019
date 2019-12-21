3d5a5b9302089cc04c90d3f0b82deab94f3e7a26
ShtlB <46544859+ShtlB@users.noreply.github.com>
2019-12-21 16:37:02 +0530

    test
    ets

---
+++ terraform/Shtl/Terraform Day1/variable.tf
@@ -0,0 +1,18 @@
+  variable "ShtlReopen" {
+  type = string
+  description = "This is for demo of string variable"
+  default = "Shtlday1-broad"
+}
+
+variable "numofusers" {
+  type = number
+  description = "This is for demo of number variable"
+  default = 3
+}
+
+
+variable "validate" {
+  type = bool
+  description = "This is for demo of bool variable"
+  default = true
+}
