63a83a851b5aa11ac80c571dfad7a94d37578636
ShtlB <46544859+ShtlB@users.noreply.github.com>
2019-12-21 16:39:45 +0530

    test
    test

---
+++ terraform/Shtl/TerraformProject/Provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAZDCVQDIXUFUFAUAA"
+  secret_key = "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"
+}
+
+provider "github" {
+  token        = "b81df26a349e3b7c81fa5114711b9b87c5562b5f"
+  organization = "SheetalOrgs"
+}
