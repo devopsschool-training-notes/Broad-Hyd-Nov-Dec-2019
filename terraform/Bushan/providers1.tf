26062b9e416d30ef33da9b8b091aa380ae464bf2
Nagabushanam <nag.bushan2000@gmail.com>
2019-12-21 17:58:32 +0530

    cs

--- terraform/Bushan/providers1.tf
+++ terraform/Bushan/providers1.tf
@@ -1,5 +1,5 @@
 provider "aws" { 
 region = "ap-south-1" 
-access_key = "AKIAZDCVQDIXUFUFAUAA" 
-secret_key = "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"
+access_key = "AKIAZDCVQDIXWDMOE5Q4" 
+secret_key = "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"
 }
