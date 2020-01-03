eed1cff989f7e4a2e79460602cbd62dabaa6a4c3
root <root@ip-172-31-9-157.ap-south-1.compute.internal>
2020-01-03 06:59:22 +0000

    my changes

---
+++ Chef/rajesh/chef-repo/first.rb
@@ -0,0 +1,13 @@
+file 'index1.html' 
+
+file 'index.html' do
+  action :delete
+end
+
+file 'file4.txt' do
+  mode                       755
+  owner                      "ec2-user"
+  action                     :create
+end
+
+directory 'src'
