1cfb2fd54c37a62b5e0314cf1559a8ae000ab090
Rajesh Kumar <devops@rajeshkumar.xyz>
2020-01-03 11:17:45 +0000

    addg

---
+++ Chef/rajesh/chef-repo/cookbooks/web/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook:: web
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+include_recipe '::service'
+#include_recipe 'service'
+
+file 'index.html' do
+  content                    "<h1>Welcome to DevOpsSchool Chef Class<h1>"
+end
+
+package 'httpd'
+
+bash 'copy index' do
+  code <<-EOH
+    cp index.html /var/www/html
+    EOH
+end
