213a43aef7dfc48886819c0e0b47aee64a66d42b
root <root@ip-172-31-7-70.ap-south-1.compute.internal>
2020-01-03 11:51:01 +0000

    add ravi folder

---
+++ Chef/Ravi/chef-repo/cookbooks/firstcb/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook:: firstcb
+# Recipe:: default
+#
+
+include_recipe '::seond'
+
+file 'file1.txt' do
+  group                      "root"
+  owner                      "ec2-user"
+  action		     :touch
+end
+
+# Copyright:: 2020, The Authors, All Rights Reserved.
